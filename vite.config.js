04d6fc169c8956a577e2c72581be219216c7a68f
Javier Quiroga <contact@javierquiroga.com>
2025-08-11 16:59:01 +0100

    feat: Add server for dev and preview

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   // base: '/base-url',
   plugins: [react()],
+  server: {
+    host: true, // Enables access from other devices on your network
+    port: 5173, // Optional: pick a custom port if needed
+  },
 });
